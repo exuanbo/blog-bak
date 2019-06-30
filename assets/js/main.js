92302dc4c2a5ae2252c08649c79faab5792772a0
exuanbo <sambercheng@gmail.com>
2019-07-01 00:54:57 +0200

    add js to fix toc reomove hr change button size

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-jQuery.cachedScript = function(url, options) {
+jQuery.cachedScript = (url, options)  => {
   // Allow user to set any option except for dataType, cache, and url
   options = $.extend( options || {}, {
     dataType: "script",
@@ -11,15 +11,55 @@ jQuery.cachedScript = function(url, options) {
   return jQuery.ajax(options);
 };
 
+// Fix toc
+//
+(() => {
+  var toc = document.getElementById('TableOfContents');
+  if (!toc) return;
+  var li, ul = toc.querySelector('ul');
+  if (ul.childElementCount !== 1) return;
+  li = ul.firstElementChild;
+  if (li.tagName !== 'LI') return;
+  // remove <ul><li></li></ul> where only <ul> only contains one <li>
+  ul.outerHTML = li.innerHTML;
+})();
+
 // Open external link in new tab
 //
-let links = $('a');
-for (var i = 0, length = links.length; i < length; i++) {
-  if (links[i].hostname != window.location.hostname) {
-      links[i].target = '_blank';
-      links[i].rel = 'noreferrer';
+(() => {
+  let links = $('a');
+  for (var i = 0, length = links.length; i < length; i++) {
+    if (links[i].hostname != window.location.hostname) {
+        links[i].target = '_blank';
+        links[i].rel = 'noreferrer';
+    }
   }
-}
+})();
+
+// Center images
+//
+(() => {
+  function center_el(tagName) {
+    let tags = document.getElementsByTagName(tagName), i, tag;
+    for (i = 0; i < tags.length; i++) {
+      tag = tags[i];
+      let parent = tag.parentElement;
+      // center an image if it is the only element of its parent
+      if (parent.childNodes.length === 1) {
+        // if there is a link on image, check grandparent
+        if (parent.nodeName === 'A') {
+          parent = parent.parentElement;
+          if (parent.childNodes.length != 1) continue;
+        }
+        if (parent.nodeName === 'P') parent.style.textAlign = 'center';
+      }
+    }
+  }
+  let tagNames = ['img', 'embed', 'object'];
+  for (let i = 0; i < tagNames.length; i++) {
+      center_el(tagNames[i]);
+  }
+})();
 
 // Load comments
 //
@@ -87,7 +127,7 @@ $(window).on('scroll', () => {
 });
 
 arrowUp.click(() => {
-  $('html,body').animate({scrollTop: '0px'}, 100);
+  $('html,body').animate({scrollTop: '0px'}, 200);
 });
 
 // Toggle toc
@@ -105,33 +145,8 @@ toc.find('a').click(() => {
   toc.fadeOut('fast');
 });
 
-$(document).mouseup(function (e) {
+$(document).mouseup((e) => {
   if (!toc.is(e.target) && !toc.has(e.target).length) {
       toc.fadeOut('fast');
   }
 });
-
-// Center images
-//
-(function() {
-  function center_el(tagName) {
-    let tags = document.getElementsByTagName(tagName), i, tag;
-    for (i = 0; i < tags.length; i++) {
-      tag = tags[i];
-      let parent = tag.parentElement;
-      // center an image if it is the only element of its parent
-      if (parent.childNodes.length === 1) {
-        // if there is a link on image, check grandparent
-        if (parent.nodeName === 'A') {
-          parent = parent.parentElement;
-          if (parent.childNodes.length != 1) continue;
-        }
-        if (parent.nodeName === 'P') parent.style.textAlign = 'center';
-      }
-    }
-  }
-  let tagNames = ['img', 'embed', 'object'];
-  for (let i = 0; i < tagNames.length; i++) {
-      center_el(tagNames[i]);
-  }
-})();
