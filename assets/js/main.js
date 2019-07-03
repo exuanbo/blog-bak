68c0b6e3f00c0e2e4728d0886b439c4aa57e3a6f
exuanbo <sambercheng@gmail.com>
2019-07-03 19:06:42 +0200

    standardized main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,39 +6,39 @@
 //
 const loadScript = (source, beforeEl, async = true, defer = true) => {
   return new Promise((resolve, reject) => {
-    let script = document.createElement('script');
-    const prior = beforeEl || document.getElementsByTagName('script')[0];
+    let script = document.createElement('script')
+    const prior = beforeEl || document.getElementsByTagName('script')[0]
 
-    script.async = async;
-    script.defer = defer;
+    script.async = async
+    script.defer = defer
 
-    function onloadHander(_, isAbort) {
+    function onloadHander (_, isAbort) {
       if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
-        script.onload = null;
-        script.onreadystatechange = null;
-        script = undefined;
+        script.onload = null
+        script.onreadystatechange = null
+        script = undefined
 
         if (isAbort) {
-          reject();
+          reject()
         } else {
-          resolve();
+          resolve()
         }
       }
     }
 
-    script.onload = onloadHander;
-    script.onreadystatechange = onloadHander;
+    script.onload = onloadHander
+    script.onreadystatechange = onloadHander
 
-    script.src = source;
-    prior.parentNode.insertBefore(script, prior);
-  });
+    script.src = source
+    prior.parentNode.insertBefore(script, prior)
+  })
 }
 
 // addEventListener helper
 //
 const listen = (ele, e, callback) => {
   if (document.querySelector(ele) !== null) {
-    document.querySelector(ele).addEventListener(e, callback);
+    document.querySelector(ele).addEventListener(e, callback)
   }
 }
 
@@ -49,78 +49,79 @@ const listen = (ele, e, callback) => {
 // Smoothly scroll
 //
 const getElementY = query => {
-  return window.pageYOffset + document.querySelector(query).getBoundingClientRect().top;
+  return window.pageYOffset + document.querySelector(query).getBoundingClientRect().top
 }
 
 const doScrolling = (element, duration) => {
-	let startingY = window.pageYOffset;
-  let elementY = getElementY(element);
+  let startingY = window.pageYOffset
+  let elementY = getElementY(element)
   // If element is close to page's bottom then window will scroll only to some position above the element.
-  let targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY;
-	let diff = targetY - startingY;
+  let targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY
+  let diff = targetY - startingY
   // Easing function: easeInOutCubic
   // From: https://gist.github.com/gre/1650294
-  let easing = t => { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 };
-  let start;
+  let easing = t => { return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1 }
+  let start
 
-  if (!diff) return;
+  if (!diff) return
 
-	// Bootstrap our animation - it will get called right before next frame shall be rendered.
-	window.requestAnimationFrame(step = timestamp => {
-    if (!start) start = timestamp;
+  // Bootstrap our animation - it will get called right before next frame shall be rendered.
+  window.requestAnimationFrame(function step (timestamp) {
+    if (!start) start = timestamp
     // Elapsed miliseconds since start of scrolling.
-    let time = timestamp - start;
-		// Get percent of completion in range [0, 1].
-    let percent = Math.min(time / duration, 1);
+    let time = timestamp - start
+    // Get percent of completion in range [0, 1].
+    let percent = Math.min(time / duration, 1)
     // Apply the easing.
     // It can cause bad-looking slow frames in browser performance tool, so be careful.
-    percent = easing(percent);
+    percent = easing(percent)
 
-    window.scrollTo(0, startingY + diff * percent);
+    window.scrollTo(0, startingY + diff * percent)
 
-		// Proceed with animation as long as we wanted it to.
+    // Proceed with animation as long as we wanted it to.
     if (time < duration) {
-      window.requestAnimationFrame(step);
+      window.requestAnimationFrame(step)
     }
-  });
+  })
 }
 
 // Animate.css
 //
 const animateCSS = (element, animationName, callback) => {
-  element.classList.add('animated', animationName);
+  element.classList.add('animated', animationName)
 
   const handleAnimationEnd = () => {
-    element.classList.remove('animated', animationName);
-    element.removeEventListener('animationend', handleAnimationEnd);
+    element.classList.remove('animated', animationName)
+    element.removeEventListener('animationend', handleAnimationEnd)
 
-    if (typeof callback === 'function') callback();
+    if (typeof callback === 'function') callback()
   }
 
-  element.addEventListener('animationend', handleAnimationEnd);
+  element.addEventListener('animationend', handleAnimationEnd)
 }
 
 // Fix toc
 //
 (() => {
-  let toc = document.getElementById('TableOfContents');
-  if (!toc) return;
-  let li, ul = toc.querySelector('ul');
-  if (ul.childElementCount !== 1) return;
-  li = ul.firstElementChild;
-  if (li.tagName !== 'LI') return;
+  let toc = document.getElementById('TableOfContents')
+  if (!toc) return
+  let li
+  let ul = toc.querySelector('ul')
+  if (ul.childElementCount !== 1) return
+  li = ul.firstElementChild
+  if (li.tagName !== 'LI') return
   // remove <ul><li></li></ul> where only <ul> only contains one <li>
-  ul.outerHTML = li.innerHTML;
+  ul.outerHTML = li.innerHTML
 })();
 
 // Open external link in new tab
 //
 (() => {
-  let links = document.getElementsByTagName('a');
+  let links = document.getElementsByTagName('a')
   for (let i = 0, length = links.length; i < length; i++) {
-    if (links[i].hostname != window.location.hostname) {
-        links[i].target = '_blank';
-        links[i].rel = 'noreferrer';
+    if (links[i].hostname !== window.location.hostname) {
+      links[i].target = '_blank'
+      links[i].rel = 'noreferrer'
     }
   }
 })();
@@ -128,130 +129,130 @@ const animateCSS = (element, animationName, callback) => {
 // Center images
 //
 (() => {
-  let center_el = (tagName) => {
-    let tags = document.getElementsByTagName(tagName), i, tag;
-    for (i = 0; i < tags.length; i++) {
-      tag = tags[i];
-      let parent = tag.parentElement;
+  let centerEl = (tagName) => {
+    let tags = document.getElementsByTagName(tagName)
+    for (let i = 0; i < tags.length; i++) {
+      let tag = tags[i]
+      let parent = tag.parentElement
       // center an image if it is the only element of its parent
       if (parent.childNodes.length === 1) {
         // if there is a link on image, check grandparent
         if (parent.nodeName === 'A') {
-          parent = parent.parentElement;
-          if (parent.childNodes.length != 1) continue;
+          parent = parent.parentElement
+          if (parent.childNodes.length !== 1) continue
         }
-        if (parent.nodeName === 'P') parent.style.textAlign = 'center';
+        if (parent.nodeName === 'P') parent.style.textAlign = 'center'
       }
     }
   }
-  let tagNames = ['img', 'embed', 'object'];
+  let tagNames = ['img', 'embed', 'object']
   for (let i = 0; i < tagNames.length; i++) {
-      center_el(tagNames[i]);
+    centerEl(tagNames[i])
   }
-})();
+})()
 
 // Load katex
 //
 if (document.getElementsByClassName('math').length !== 0) {
-  const katexUrl = '/js/katex/katex.min.js';
-  const autoRenderUrl = '/js/katex/contrib/auto-render.min.js';
+  const katexUrl = '/js/katex/katex.min.js'
+  const autoRenderUrl = '/js/katex/contrib/auto-render.min.js'
 
   loadScript(katexUrl).then(() => {
     loadScript(autoRenderUrl).then(() => {
-      renderMathInElement(document.body);
+      renderMathInElement(document.body)
     })
-  });
+  })
 }
 
 // Load comments
 //
-let comments = document.getElementById('vcomments');
+let comments = document.getElementById('vcomments')
 if (comments) {
-  let commentsLoader = document.getElementById('comments-loader');
+  let commentsLoader = document.getElementById('comments-loader')
 
-  const valineJsUrl = '/js/Valine.min.js';
+  const valineJsUrl = '/js/Valine.min.js'
 
   const loadComments = () => {
     loadScript(valineJsUrl).then(() => {
-        new Valine({
-          el: '#vcomments',
-          appId: '89VYThwE6PdkAYLdYXE8jIMK-MdYXbMMI',
-          appKey: 'cq3bSJa9tmdhLuTQ7PT6rpzM',
-          notify: false,
-          verify: false,
-          avatar: 'hide',
-          placeholder: '说点什么吧...'
-        });
-        animateCSS(commentsLoader, 'fadeOutDown', () => {
-          commentsLoader.style.display = 'none';
-          comments.style.display = 'block';
-          animateCSS(comments, 'fadeInDown');
-        });
-      }, () => {
-        console.log('Failed to load Valine.min.js');
-      });
+      var valine = new Valine()
+      valine.init({
+        el: '#vcomments',
+        appId: '89VYThwE6PdkAYLdYXE8jIMK-MdYXbMMI',
+        appKey: 'cq3bSJa9tmdhLuTQ7PT6rpzM',
+        notify: false,
+        verify: false,
+        avatar: 'hide',
+        placeholder: '说点什么吧...'
+      })
+      animateCSS(commentsLoader, 'fadeOutDown', () => {
+        commentsLoader.style.display = 'none'
+        comments.style.display = 'block'
+        animateCSS(comments, 'fadeInDown')
+      })
+    }, () => {
+      console.log('Failed to load Valine.min.js')
+    })
   }
 
-  listen('#comments-loader', 'click', loadComments);
+  listen('#comments-loader', 'click', loadComments)
 }
 
 // Back to top
 //
-let arrowUp = document.getElementById('top');
-let showArrow = false;
-let clickArrow = false;
+let arrowUp = document.getElementById('top')
+let showArrow = false
+let clickArrow = false
 
 window.addEventListener('scroll', () => {
   if (window.pageYOffset > 200) {
-      arrowUp.style.display = 'block';
-      if (showArrow == false) {
-        animateCSS(arrowUp, 'bounceInUp', () => {
-          showArrow = true;
-        });
-      }
-  }
-  else if (showArrow == true && clickArrow == false) {
+    arrowUp.style.display = 'block'
+    if (showArrow === false) {
+      animateCSS(arrowUp, 'bounceInUp', () => {
+        showArrow = true
+      })
+    }
+  } else if (showArrow === true && clickArrow === false) {
     animateCSS(arrowUp, 'fadeOutUp', () => {
-      arrowUp.style.display = 'none';
-      showArrow = false;
-    });
+      arrowUp.style.display = 'none'
+      showArrow = false
+    })
   }
-});
+})
 
 listen('#top', 'click', () => {
-  clickArrow = true;
-  doScrolling('h1', 500);
+  clickArrow = true
+  doScrolling('h1', 500)
   animateCSS(arrowUp, 'bounceOutUp', () => {
-    arrowUp.style.display = 'none';
-    showArrow = false;
-    clickArrow = false;
-  });
-});
+    arrowUp.style.display = 'none'
+    showArrow = false
+    clickArrow = false
+  })
+})
 
 // Toggle toc
 //
-let toc = document.getElementById('TableOfContents');
+let toc = document.getElementById('TableOfContents')
 if (toc) {
-  let tocA = toc.querySelectorAll('a');
+  let tocA = toc.querySelectorAll('a')
 
   const toggleToc = () => {
-    if (window.getComputedStyle(toc,null).getPropertyValue('display') == 'none' || toc.style.display == 'none') {
-      toc.style.display = 'block';
-      animateCSS(toc, 'bounceInLeft');
+    if (window.getComputedStyle(toc, null).getPropertyValue('display') === 'none' || toc.style.display === 'none') {
+      toc.style.display = 'block'
+      animateCSS(toc, 'bounceInLeft')
     } else {
       animateCSS(toc, 'bounceOutLeft', () => {
-        toc.style.display = 'none';
-      });
+        toc.style.display = 'none'
+      })
     }
   }
 
   listen('#toggle-toc', 'click', () => {
-    toggleToc();
-  });
+    toggleToc()
+  })
 
   tocA.forEach(a => {
     a.addEventListener('click', () => {
-      toggleToc();
+      toggleToc()
     })
-  });
+  })
 }
