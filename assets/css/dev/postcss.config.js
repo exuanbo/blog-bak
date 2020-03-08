ed41430e185571b0bca4a66ee64040cff0ad96b6
exuanbo <sambercheng@gmail.com>
2020-03-08 14:38:29 +0100

    tailwindcss init

---
+++ assets/css/dev/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    require('postcss-import')({
+      path: [__dirname]
+    }),
+    require('tailwindcss')('assets/css/tailwind.config.js'),
+    require('autoprefixer')
+  ]
+}
---
+++ assets/css/postcss.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [
+    require('postcss-import')({
+      path: [__dirname]
+    }),
+    require('tailwindcss')('assets/css/tailwind.config.js'),
+    // Configuration of purgecss for Tailwindcss
+    // see https://tailwindcss.com/docs/controlling-file-size/#setting-up-purgecss
+    require('@fullhuman/postcss-purgecss')({
+      // Specify the paths to all of the template files in your project
+      content: [
+        'layouts/**/*.html'
+      ],
+      // Include any special characters you're using in this regular expression
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+      fontFace: true
+    }),
+    require('autoprefixer')({
+      grid: true
+    }),
+    require('postcss-reporter')
+  ]
+}
