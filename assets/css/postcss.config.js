5d64293d4eb6f22434f195f29944793a84cddaf7
exuanbo <sambercheng@gmail.com>
2020-03-09 14:03:41 +0100

    rewrite theme with tailwindcss

--- assets/css/postcss.config.js
+++ assets/css/postcss.config.js
@@ -9,7 +9,8 @@ module.exports = {
     require('@fullhuman/postcss-purgecss')({
       // Specify the paths to all of the template files in your project
       content: [
-        'layouts/**/*.html'
+        'layouts/**/*.html',
+        'public/**/*.html'
       ],
       // Include any special characters you're using in this regular expression
       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   theme: {
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    extend: {
+      colors: {
+        shironeri: '#FCFAF2',
+        'shironeri-shade': '#F8F4E2',
+        aimirucha: '#0F4C3A',
+        hiwamoegi: '#90B44B'
+      },
+      boxShadow: {
+        a: 'inset 0 -4px 0 #DAC9A6',
+        'a-hover': 'inset 0 -8px 0 #DAC9A6'
+      },
+      opacity: {
+        40: '.4',
+        60: '.6'
+      }
+    }
+  }
 }
