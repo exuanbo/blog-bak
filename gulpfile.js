bde506c3762f766ee20f8905867927284f611810
exuanbo <sambercheng@gmail.com>
2020-04-16 10:25:04 +0800

    add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const del = require('del')
-const { src, dest, series } = require('gulp')
+const { src, dest, series, watch } = require('gulp')
 const uglify = require('gulp-uglify-es').default
 const { execFile } = require('child_process')
 const hugo = require('hugo-bin')
 const htmlmin = require('gulp-htmlmin')
+const browserSync = require('browser-sync').create()
 
 function clean() {
   return del(['assets/build', 'public'])
@@ -16,20 +17,43 @@ function js() {
 }
 
 function build(cb) {
-  return execFile(hugo, ['--gc'], (error, stdout) => {
-    if (error) cb(error)
-    console.log('\n' + stdout)
-  })
+  return execFile(hugo, ['--gc'], (error) => cb(error))
 }
 
 function html() {
   return src('public/**/*.html', { base: '.' })
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      conservativeCollapse: true
-    }))
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+      })
+    )
     .pipe(dest('.'))
+}
+
+function server() {
+  browserSync.init({
+    server: {
+      baseDir: './public',
+    },
+  })
+  watch(
+    [
+      'assets/**',
+      '!assets/build/**',
+      'content/**',
+      'layouts/**',
+      'static/**',
+      'config.toml',
+    ],
+    { ignoreInitial: false },
+    series('default')
+  )
+  watch('public/**').on('change', browserSync.reload)
 }
 
 exports.default = series(clean, js, build, html)
 exports.js = js
+exports.build = build
+exports.html = html
+exports.server = server
