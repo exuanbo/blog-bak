b93694dd4bc70030ae0c6f37ae7ef103c3a95c18
exuanbo <exuanbo@protonmail.com>
2020-04-27 23:51:20 +0800

    update js

--- assets/js/katex.js
+++ assets/js/katex.js
@@ -1,5 +1,4 @@
-loadScript('https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js').then(
-  loadScript('https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/contrib/auto-render.min.js').then(
-    renderMathInElement(document.body)
-  )
-)
+loadScript('https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.js')
+loadScript('https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/contrib/auto-render.min.js').then(() => {
+  renderMathInElement(document.body)
+})
--- assets/js/medium-zoom.js
+++ assets/js/medium-zoom.js
@@ -1,11 +1,3 @@
 loadScript('https://cdn.jsdelivr.net/npm/medium-zoom@1.0.5/dist/medium-zoom.min.js').then(() => {
-  const imgObserver = new IntersectionObserver(entries => {
-    if (entries[0].isIntersecting) {
-      mediumZoom('img', { background: 'rgba(0, 0, 0, 0.5)' })
-      imgObserver.disconnect()
-    }
-  }, {
-    threshold: [0]
-  })
-  imgObserver.observe(document.getElementsByTagName('img')[0])
+  mediumZoom('img', { background: 'rgba(0, 0, 0, 0.5)' })
 })
