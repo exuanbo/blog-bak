6ce6e5d3dcf37336804459a32d50565899707a04
exuanbo <exuanbo@protonmail.com>
2021-08-08 16:40:46 +0100

    fix: ackee tracker setup

--- assets/js/ackee.js
+++ assets/js/ackee.js
@@ -1,10 +1,7 @@
-loadScript("https://ackee.exuanbo.xyz/privacy-respected.js", {
+loadScript('https://ackee.exuanbo.xyz/privacy-respected.js', {
   async: true
 }).then(() => {
   ackeeTracker
-    .create({
-      server: "https://ackee.exuanbo.xyz",
-      domainId: "1f9f76ad-23e7-4b4a-9550-af63af44a4bd"
-    })
-    .record()
+    .create({ server: 'https://ackee.exuanbo.xyz' })
+    .record('1f9f76ad-23e7-4b4a-9550-af63af44a4bd')
 })
