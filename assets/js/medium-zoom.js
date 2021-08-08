9640fb9d18bb5f0acd604c154ee3b2cf3375564e
exuanbo <exuanbo@protonmail.com>
2021-08-08 16:40:59 +0100

    style: format with prettier

--- assets/js/medium-zoom.js
+++ assets/js/medium-zoom.js
@@ -1,5 +1,5 @@
 loadScript(
-  "https://cdn.jsdelivr.net/npm/medium-zoom@1.0.5/dist/medium-zoom.min.js"
+  'https://cdn.jsdelivr.net/npm/medium-zoom@1.0.5/dist/medium-zoom.min.js'
 ).then(() => {
-  mediumZoom("img", { background: "rgba(0, 0, 0, 0.5)" })
+  mediumZoom('img', { background: 'rgba(0, 0, 0, 0.5)' })
 })
