f6118e14cc0759815d5d771bb70fae33247154c3
exuanbo <exuanbo@protonmail.com>
2021-08-31 18:45:00 +0100

    fix: ackee tracker setup

--- assets/js/ackee.js
+++ assets/js/ackee.js
@@ -2,6 +2,6 @@ loadScript('https://ackee.exuanbo.xyz/privacy-respected.js', {
   async: true
 }).then(() => {
   ackeeTracker
-    .create({ server: 'https://ackee.exuanbo.xyz' })
+    .create('https://ackee.exuanbo.xyz')
     .record('1f9f76ad-23e7-4b4a-9550-af63af44a4bd')
 })
